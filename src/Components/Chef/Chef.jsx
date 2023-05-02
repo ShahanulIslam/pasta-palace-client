5c5b74298fc63c55b25ebdd0cc7fcaa85c2f77b2
Your Name <shahanricky86@gmail.com>
2023-05-02 23:54:43 +0600

    footer

--- src/Components/Chef/Chef.jsx
+++ src/Components/Chef/Chef.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { AiFillLike } from "react-icons/ai";
 
 const Chef = ({ chef }) => {
     const { name, picture, years_of_experience, likes, num_of_recipes
-    } = chef
+    } = chef;
 
-    console.log(chef)
+
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl p-5">
             <figure><img src={picture} alt="Shoes" /></figure>
@@ -12,7 +13,7 @@ const Chef = ({ chef }) => {
                 <h2 className="card-title justify-center">{name}</h2>
                 <div className='flex'>
                     <p>{years_of_experience} Years Experience </p>
-                    <p>{likes}</p>
+                    <p className='flex justify-center items-center'><AiFillLike className='me-1'></AiFillLike>{likes}</p>
                     <p>Recipes : {num_of_recipes}</p>
                 </div>
                 <div className="card-actions justify-center mt-5">
