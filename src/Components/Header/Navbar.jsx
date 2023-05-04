c4fb3ad6d1bcba435e09691ff0c21ebeb7dba70a
Your Name <shahanricky86@gmail.com>
2023-05-04 20:21:09 +0600

    some-change in navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -11,34 +11,28 @@ const Navbar = () => {
   }
 
   return (
-    <div className="navbar bg-base-100 w-11/12 mx-auto">
+    <div className="navbar  mx-auto bg-sky-200">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-              </a>
-            </li>
-            <li><a>Item 3</a></li>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/blog'>Blog</Link></li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">Pasta Palace</a>
+        <Link to='/' className="btn btn-ghost normal-case text-2xl md:text-3xl">Pasta Palace</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 gap-4">
-          <NavLink className={({ isActive }) => isActive ? 'font-bold bg-indigo-500 text-white' : ''} to="/"><li><a>Home</a></li></NavLink>
-          <NavLink className={({ isActive }) => isActive ? 'font-bold bg-indigo-500 text-white' : ''} to="/blog"><li><a>Blog</a></li></NavLink>
+        <ul className="menu menu-horizontal px-1 font-semibold space-x-6">
+          <NavLink className={({isActive}) => isActive ? 'font-bold bg-indigo-500 text-white' : ''} to='/'>Home</NavLink>
+          <NavLink className={({isActive}) => isActive ? 'font-bold bg-indigo-500 text-white' : ''} to='/blog'>Blog</NavLink>
         </ul>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end flex items-center gap-5">
         <div>{user ?
-          <div className='flex gap-5'>
+          <div className='flex gap-3 md:gap-5'>
             <img title={user.displayName} className='rounded-full w-11 h-11' src={user.photoURL} alt="User" />
             <button onClick={handleLogOut} className='btn'>Logout</button>
           </div> :
@@ -46,8 +40,9 @@ const Navbar = () => {
         }</div>
 
       </div>
-
     </div>
+
+
   );
 };
 
